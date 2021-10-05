e442fe23ec92fa51e9cd784d36b10a5c2308f379
Lev <lyoha.bel2020@gmail.com>
2021-10-05 11:58:07 +0300

    adaptive updated

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -37,14 +37,23 @@
           placeholder="Add a new task"
         />
 
-        <!--<button class="todo-add">Add a new task</button>-->
-
         <div class="todo-filter">
           <span class="todo-left">1/3 left</span>
           <div class="todo-wrapper">
-            <button class="todo-all filter-used">All</button>
-            <button class="todo-active">Active</button>
-            <button class="todo-completed">Completed</button>
+            <label>
+              <input type="radio" name="tasks" id="all" checked />
+              <span>All</span>
+            </label>
+
+            <label>
+              <input type="radio" name="tasks" id="active" />
+              <span>Active</span>
+            </label>
+
+            <label>
+              <input type="radio" name="tasks" id="completed" />
+              <span>Completed</span>
+            </label>
           </div>
         </div>
       </div>
@@ -60,34 +69,50 @@ export default {
 
 <style lang="scss">
 .todo {
-  margin: 0 auto 0 auto;
-  max-width: 74rem;
-  min-height: 100vh;
-  //@include mobile {
-  //	max-width: 900px;
-  //}
+  background-image: $main-bgi;
 }
 
 .todo-image {
-  width: 100%;
-  //height: 100%;
+  margin: 0 auto 0 auto;
+  max-width: 74rem;
+  padding-top: 6rem;
   min-height: 100vh;
   background-image: url(../assets/images/background.png);
   background-repeat: no-repeat;
   background-position: 41rem 4rem;
-  position: relative;
+  @include desktop {
+    background-position: 35rem 4rem;
+  }
+  @include tablet-landscape {
+    background-position: 30rem 4rem;
+  }
+  @include tablet-small {
+    background-position: 20rem 1rem;
+  }
+  @include tablet-small-2 {
+    background-position: 10rem 1rem;
+  }
 }
 
 .todo-area {
-  position: absolute;
   width: 34rem;
-  left: 19.5rem;
-  top: 6rem;
-
+  margin: 0 auto 0 auto;
   background: $bg-light;
   border: 0.125rem solid $bg-title;
   box-sizing: border-box;
   box-shadow: 0px 0.5rem 2.2rem 0.3rem $shadow;
+  @include tablet-portrait {
+    width: 32rem;
+  }
+  @include mobile-big {
+    width: 28rem;
+  }
+  @include mobile {
+    width: 25rem;
+  }
+  @include mobile-small {
+    width: 20rem;
+  }
 }
 
 .todo-title {
@@ -101,6 +126,9 @@ export default {
 .todo-list {
   padding: 2rem;
   padding-bottom: 0;
+  @include tablet-portrait {
+    padding: 2rem 1rem;
+  }
 
   li {
     @extend %task;
@@ -112,6 +140,9 @@ export default {
       display: flex;
       align-items: center;
       color: $font-dark;
+      @include mobile-small {
+        font-size: 1rem;
+      }
     }
   }
 }
@@ -201,6 +232,20 @@ export default {
   display: flex;
   align-items: center;
   color: $font-dark;
+  @include tablet-portrait {
+    width: 29.5rem;
+    margin-left: 1rem;
+  }
+  @include mobile-big {
+    width: 25.5rem;
+  }
+  @include mobile {
+    width: 22.5rem;
+  }
+  @include mobile-small {
+    width: 17.5rem;
+    font-size: 1rem;
+  }
 }
 
 .todo-add::placeholder {
@@ -217,6 +262,9 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  @include mobile {
+    flex-direction: column;
+  }
 }
 
 .todo-left {
@@ -224,21 +272,29 @@ export default {
 }
 
 .todo-wrapper {
-  button {
-    @include filter-font;
-    border: 0;
-    background: transparent;
-  }
+  display: flex;
+  align-items: center;
+  label {
+    display: flex;
+    span {
+      display: block;
+      @include filter-font;
+      margin-right: 1.5rem;
+      padding: 0.3rem 0.625rem;
+    }
+    #completed ~ span {
+      margin-right: 0;
+    }
+    input {
+      appearance: none;
+    }
 
-  button:not(:last-child) {
-    margin-right: 1.5rem;
-  }
-
-  .filter-used {
-    padding: 0.3rem 0.625rem;
-    border: 1px solid $filter;
-    box-sizing: border-box;
-    border-radius: 0.625rem;
+    input:checked ~ span {
+      padding: 0.3rem 0.5625rem;
+      border: 0.0625rem solid $filter;
+      box-sizing: border-box;
+      border-radius: 0.625rem;
+    }
   }
 }
 </style>
